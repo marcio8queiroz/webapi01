abf68b0ca870adbb59c7a5f32d2a80c98d90ff5f
marcio8queiroz <marcio8queiroz@hotmail.com>
2024-10-19 15:57:40 -0400

    fs

--- db.js
+++ db.js
@@ -1,37 +1,53 @@
 const {v4} = require("uuid");
-global.users = [];
+const fs = require("fs");
+const FILE_PATH = require("path").join(__dirname, "users.json");
+ 
+//global.users = [];
 
 
 
 function findUsers(){
-    return global.users;
+    try{
+    return require("./users.json");
+    }
+    catch(ex){
+        return [];
+    }
+   // return global.users;
 }
 
 function findUser(id){
-    return global.users.find(item => item.id === id);
+    return findUsers.find(item => item.id === id);
 }
 
 function insertUser(user){
+    const users = findUsers();
     user.id = v4();
-    global.users.push(user);
+    users.push(user);
+    fs.writeFileSync(FILE_PATH, JSON.stringify(users));
     return user;
 }
 
 function updateUser(id, user){
-    return global.users.forEach((item, index, array) => {
+    const users = findUsers();
+    users.forEach((item, index, array) => {
         if(item.id === id){
             user.id = id;
             array[index] = user;
         }
     })
-        
-    }
+    fs.writeFileSync(FILE_PATH, JSON.stringify(users));
+    return user;        
+}
 
 function deleteUser(id){
-    return global.users.forEach((item, index, array) => {
+    const users = findUsers();
+    users.forEach((item, index, array) => {
         if(item.id == id)
             array.splice(index, 1);
     })
+    fs.writeFileSync(FILE_PATH, JSON.stringify(users));
+    return id;
 }
 
 module.exports = {
